270c98551696ff08a71e85244e2c1d7853b6ba7f
Piyush <piyushkr172@gmail.com>
2023-06-08 21:25:48 +0530

    First Commit

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "hello world" << endl;
+
+    // string userName;
+    // cout << "Enter Your Name" << endl;
+    // getline(cin, userName);
+    // cout << "Your Name is : " << userName;
+
+    double a = 0, b = 0;
+    cin >> a >> b;
+    cout << a << "+" << b << ": " << a + b << endl;
+
+    return 0;
+}
